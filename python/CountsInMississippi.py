effbee403e1d41bc11d7cf27c90588caf33a5e25
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-04-11 18:20:11 -0400

    Update CountsInMississippi.py

--- python/CountsInMississippi.py
+++ python/CountsInMississippi.py
@@ -4,7 +4,7 @@ def find_s(str):
   if len(str) == 0 or str is None:
     return "Empty String"
   count = 0
-  for i in str:
+  for i in str.lower(): # Or upper depending on 'S' or 's'
     if i == 's':
       count += 1
   return count
@@ -12,6 +12,7 @@ def find_s(str):
 print(find_s('mississippi'))
 print(find_s('facebook'))
 print(find_s('miSsisSippi'))
-print(find_s(''))
+print(find_s(' '))
 print(find_s(' s '))
 print(find_s(''))
+print(find_s('facebook interview for Data Engineer'))
