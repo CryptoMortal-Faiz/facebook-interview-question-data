153dbdcd1add1ab5e3c0204e9e1a228416310379
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-04-12 16:31:54 -0400

    Update nth_max_dict.py

--- python/nth_max_dict.py
+++ python/nth_max_dict.py
@@ -12,6 +12,14 @@ def get_dict_max_val(new_dict):
     max_val = res[len(res)-1]
     return max_val
 
+## Approach 2
+def get_dict_max_val(new_dict):
+    if not new_dict:
+        return("Empty Dict")
+    values = set(my_dict.values())
+    res = sorted(values,reverse = True)
+    return res[0]
+
 # Driver
-dict1 ={"google":12, "amazon":9, "flipkart":4, "gfg":13}
+dict1 ={"google":12, "amazon":9, "flipkart":4, "fb":13}
 print(get_dict_max_val(dict1))
