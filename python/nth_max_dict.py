6e208cd0d3d29aca5a8d426c2fbc458330d6f5d6
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-04-12 16:13:47 -0400

    Update nth_max_dict.py

--- python/nth_max_dict.py
+++ python/nth_max_dict.py
@@ -1,28 +1,17 @@
 # Given a ´dictionary, print the key for nth highest value present in the dict
 
-new_dict ={"google":12, "amazon":9, "flipkart":4, "gfg":13}
- 
-# maximum value
-max = max(new_dict.values())
- 
-# iterate through the dictionary
-max2 = 0
-for v in new_dict.values():
-     if(v>max2 and v<max):
-            max2 = v
+def get_dict_max_val(new_dict):
+    if not new_dict:
+        return("Empty Dict")
+    val_list = list(new_dict.values())
+    res = []
+    for i in val_list:
+        if i not in res:
+            res.append(i)
+    res.sort
+    max_val = res[len(res)-1]
+    return max_val
 
-# key_list = list(my_dict.keys())
-# val_list = list(my_dict.values())
- 
-# # print key with val 100
-# position = val_list.index(100)
-# print(key_list[position])
- 
-# # print key with val 112
-# position = val_list.index(112)
-# print(key_list[position])
- 
-# # one-liner
-# print(list(my_dict.keys())[list(my_dict.values()).index(112)]) 
-# print the second largest value
-print(max2)
+# Driver
+dict1 ={"google":12, "amazon":9, "flipkart":4, "gfg":13}
+print(get_dict_max_val(dict1))
