aa03cb8eb2787efca6406e29abe9c464f16e0319
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-04-14 00:25:47 -0400

    Update twoSum.py

--- python/twoSum.py
+++ python/twoSum.py
@@ -5,6 +5,14 @@ def two_sum(arr,target):
         for j in range(0,len(arr)):
             if arr[i] + arr[j] == target:
                 return [arr[i],arr[j]]
+            
+## Easier
+def two_sum(arr,target):
+    diff = 0
+    for i in range(0,len(arr)):
+        diff = target - arr[i]
+        if diff in arr:
+            return [i,arr.index(diff)]
 
 
 print(two_sum([2,3,4,5],9))
