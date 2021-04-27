444ea4c717a5c889ab52b548e04bfb4f09c36e39
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-04-26 22:21:49 -0400

    Update allques.sql

--- sql/allques.sql
+++ sql/allques.sql
@@ -1,3 +1,39 @@
+products
++---------------------------+
+| product_id | int |
+| product_class_id | int |
+| brand_name | varchar | |
+| product_name | varchar |
+| price | int | 
+---------------------
+sales
+| product_id | int |
+| store_id | int |
+| customer_id | int |
+| promotion_id | int |
+| store_sales | decimal |
+| store_cost | decimal |
+| units_sold | decimal |
+| transaction_date | date 
+---------------------
+stores
+| store_id | int |
+| type | varchar  | 
+| name | varchar  | 
+| state | varchar | 
+| first_opened_date | datetime|
+| last_remodel_date | datetime|
+| area_sqft | int |
+---------------------------------
+customers
+| customer_id | int |
+| first_name | varchar |
+| state | varchar |
+| birthdate | date |
+| gender | varchar |
+| date_account_opened | date |
+
+
 --  What brands have an average price above $3 and contain at least 2 different products?
 select brand_name
 from product
@@ -27,5 +63,10 @@ select customer_id, count(distinct brandname) from sales s inner join products p
 on s.product_id = p.product_id
 where p.brand_name in ("fort west", " Golden")
 group by customer_id having count(distinct brandname ) = 2
+
+-- given table products(product_id, brand_name,product_name, product_price), find brand_names with avg price > 3 and having more then 5 products
+select distinct brand_names from sales s inner join products p
+on s.product_id = p.product_id
+group by brand_names having AVG(price) > 3 and count(distinct product_id) > 5
 
 
