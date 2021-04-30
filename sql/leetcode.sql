45590e3d84650f6aef8ac71bda00029ad8645026
CryptoMortal-Faiz <73511145+CryptoMortal-Faiz@users.noreply.github.com>
2021-04-30 16:12:14 -0400

    Update leetcode.sql

--- sql/leetcode.sql
+++ sql/leetcode.sql
@@ -4,6 +4,7 @@ on p.sub_id = s.parent_id
 where p.parent_id is null
 group by p.sub_id
 ---------------------------------------------------------------------------------------------------
+
 -- Ads performance
 https://leetcode.com/problems/ads-performance/
 SELECT ad_id, 
@@ -12,6 +13,7 @@ FROM Ads
 GROUP BY ad_id 
 ORDER BY ctr DESC,ad_id ASC
 ---------------------------------------------------------------------------------------------------
+
 -- Customer who bought A,B but not C
 select  C.customer_id, C.customer_name
 from Orders O
@@ -43,10 +45,12 @@ left join cte on cte.customer_id = c.customer_id
 where cte.score =  2
 
 ---------------------------------------------------------------------------------------------------
+
 -- Emplloyees earning more tha thier managers
 select e1.Name as Employee from Employee e1, Employee e2 where e2.Id = e1.ManagerID and e1.salary > e2.salary and e1.ManagerID is not null
 
 ---------------------------------------------------------------------------------------------------
+
 -- n facebook, there is a follow table with two columns: followee, follower.Please write a sql query to get the amount of each follower’s follower if he/she has on
 select f.followee as follower, count(distinct f.follower) as num
 from follow f
@@ -58,6 +62,7 @@ join
 group by 1
 order by 1
 ---------------------------------------------------------------------------------------------------
+
 -- Write an SQL query that reports all the projects that have the most employees
 SELECT project_id
 FROM
@@ -73,9 +78,21 @@ GROUP BY project_id )
 SELECT project_id
 FROM t
 WHERE N = (SELECT MAX(N) FROM t)
+
 ---------------------------------------------------------------------------------------------------
 -- Avg experience years of employees of each project rounded to 2 digits
 select project_id, round(avg(experience_years), 2) as average_years
 from project p
 join employee e on p.employee_id = e.employee_id
 group by 1
+
+---------------------------------------------------------------------------------------------------
+-- Write an SQL query that reports the most experienced employees in each project. In case of a tie, report all employees with the maximum number of experience years.
+with cte as (
+SELECT a.project_id, a.employee_id,
+RANK() OVER(partition by project_id order by experience_years DESC) as rnk
+FROM Project a JOIN EMployee b ON a.employee_id = b.employee_id)
+SELECT distinct a.project_id, a.employee_id
+FROM cte JOIN Project a ON cte.employee_id = a.employee_id
+and cte.project_id = a.project_id
+where rnk = 1;
